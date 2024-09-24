63415fb56a8343e365c7ec0f688ba72aea07cd16
Ak627 <91678317+Ak627@users.noreply.github.com>
2024-09-24 10:05:30 -0600

    Add files via upload

---
+++ BinAdder.py
@@ -0,0 +1,68 @@
+#getting input
+Bin1 = input("Give me 1 binary number: ")
+Bin2 = input("Give me a binary number you want to add to the first: ")
+b1 = []
+b2 = []
+#uploading input into lists, converting to strings
+for i in Bin1:
+    b1.append(i)
+for i in Bin2:
+    b2.append(i)
+#Reverses the lists so we can read binary from left to right
+b1.reverse()
+b2.reverse()
+#set up variables
+sclr = 1
+Dec1 = 0
+Dec2 = 0
+#read through the list and check for a 1, if slot contains 1 then you will add the scalar to the Decimal value
+#multiple the scalar by 2
+for j in range(len(b1)):
+    if b1[j] == "1":
+        Dec1 = Dec1 + sclr
+    sclr = sclr * 2
+    if b1[j] != "1" and b1[j] != "0":#check if the binary number contains anything other than 1 or 0
+        print("Not a binary number, cannot continue!")
+        exit()#if contains something other than 1 or 0 end the code
+#reset the scalar
+sclr = 1
+
+
+for j in range(len(b2)):
+    if b2[j] == "1":
+        Dec2 = Dec2 + sclr
+    sclr = sclr * 2
+    if b2[j] != "1" and b2[j] != "0":
+        print("Not a binary number, cannot continue!")
+        exit()
+
+print(Dec1)
+print(Dec2)
+Dec3 = Dec1 + Dec2#add the two original decimal values into a 3rd value
+sclr =1
+slot = 0
+while(sclr <= Dec3):
+    sclr = sclr*2
+    slot = slot +1
+    
+sclr = sclr/2
+print(Dec3)
+
+print(sclr)
+
+b3 = [None] * slot#creates a list that is the lenght of the slots we need, and contains arbitrary values(None)
+print()
+for i in range(slot):
+    print(Dec3, sclr)
+    if  (Dec3 // sclr)%2 == 0:
+        b3[i] = '0'
+        Dec3 = Dec3-sclr
+        sclr = sclr/2
+    else:
+        b3[i] = '1'
+        sclr = sclr/2
+    
+b1.reverse()
+b2.reverse()
+print()
+print(b1, "+", b2, "=", b3)
