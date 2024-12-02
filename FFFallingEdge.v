6822fc7275c1170bc050f9827876e7a3900e490a
Samuel "Sam" J Engelbert <samengelbert22@gmail.com>
2024-12-02 11:34:36 -0700

    Update FFFallingEdge.v

--- FFFallingEdge.v
+++ FFFallingEdge.v
@@ -1,6 +1,6 @@
 //___________________________________________________________________
 // Falling Edge Flip Flop (D FF) code
-// Author: Sam Engelbert CS1400 Team 5 (Alex, Sam, Dawson, and Brian)
+// Author: Sam Engelbert CS1400 Team 5 (Alex, Sam, Dawson)
 //___________________________________________________________________
 module FallingEdge_DFlipFlop(D,clk,Q); 
 input D; // data 
